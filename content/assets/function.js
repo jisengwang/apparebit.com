258e515a72637206bc6444017b73aedd67c7d1d1
Robert Grimm <apparebit@users.noreply.github.com>
2019-08-10 01:28:13 -0400

    Add support for printed references
    Add script to extract hyperlinks from main article.
    Add styling so that numbered hyperlinks look reasonable.
    
    
    Former-commit-id: 75b8fdb68f4ca306fae8ecc1736c5f32987de036

---
+++ content/assets/function.js
@@ -0,0 +1,44 @@
+/* ========================================================================== *
+ * (C) Copyright 2019 Robert Grimm
+ * ========================================================================== */
+
+function addReferencesFooter() {
+  var article = document.querySelector('main > article');
+  if (!article || article.querySelector('footer.references')) return;
+
+  var footer = document.createElement('footer');
+  footer.classList.add('references');
+
+  var h2 = document.createElement('h2');
+  h2.innerText = 'References';
+  var div = document.createElement('div');
+  div.classList.add('heading');
+  div.appendChild(h2);
+  footer.appendChild(div);
+
+  var ol = document.createElement('ol');
+  footer.appendChild(ol);
+
+  var hyperlinks = article.querySelectorAll('a[href]');
+  for (var i=0; i<hyperlinks.length; i++) {
+    var href = hyperlinks[i].href;
+    if (!href.startsWith('https://apparebit.com')) {
+      var link = document.createElement('a');
+      link.href = href;
+      link.innerText = href;
+      var li = document.createElement('li');
+      li.appendChild(link);
+      ol.appendChild(li);
+    }
+  }
+
+  article.appendChild(footer);
+}
+
+window.onbeforeprint = addReferencesFooter;
+var queries = window.matchMedia('print');
+queries.addListener(function(mql) {
+  if (mql.matches) {
+    addReferencesFooter();
+  }
+});
