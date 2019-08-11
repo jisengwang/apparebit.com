20d1e79acb282713df082b51f3561121ca0f605c
Robert Grimm <apparebit@users.noreply.github.com>
2019-08-11 02:52:04 -0400

    Make build script more accessible
    • Add comments and intermediary variables to provide more scaffolding
    • Pretty-print a summary of execution for debugging etc
    
    
    Former-commit-id: f4cf137b46a6620e5d3e07381e10a705ab16be41

--- scripts/build.js
+++ scripts/build.js
@@ -4,8 +4,10 @@
 const {
   copyFile, mkdir, readdir, readFile, writeFile
 } = require('fs').promises;
+
+const chalk = require('chalk');
+const { extname, join, resolve } = require('path');
 const { promisify } = require('util');
-const { resolve } = require('path');
 const rimraf = promisify(require('rimraf'));
 
 const postcss = require('postcss')([
@@ -16,8 +18,8 @@ const postcss = require('postcss')([
   }),
 ]);
 
-const CONTENT_PATH = 'content';
-const BUILD_PATH = 'build';
+const SOURCE_ROOT = 'content';
+const TARGET_ROOT = 'build';
 
 const DIRECTORIES_TO_COPY = [
   'about',
@@ -38,39 +40,83 @@ const STYLES = [
   'assets/form.css',
 ];
 
-const MODULES = [
+const SCRIPTS = [
   'assets/function.js',
 ]
 
+const TOOL  = chalk.grey('[build]');
+const COPY  = Symbol(' copy');
+const MKDIR = Symbol('mkdir');
+const STYLE = Symbol('style');
+
+function logFileOp(operation, ...files) {
+  const names = files.map(name => {
+    const quoted = `"${name}"`;
+    switch (extname(name)) {
+      case '.html':
+        return chalk.blue(quoted);
+      case '.jpg':
+      case '.png':
+      case '.svg':
+        return chalk.cyan(quoted);
+      case '.woff':
+      case '.woff2':
+        return chalk.green(quoted);
+      default:
+        return chalk.gray(quoted);
+    }
+  });
+
+  const op = operation.description;
+  if (op === 'mkdir') console.error(TOOL);
+  console.error(TOOL, op, names[names.length - 1]);
+}
+
 async function build() {
-  await rimraf(BUILD_PATH);
-  await mkdir(BUILD_PATH);
+  const source = resolve(SOURCE_ROOT);
+  const target = resolve(TARGET_ROOT);
+
+  logFileOp(MKDIR, target);
+  await rimraf(target);
+  await mkdir(target);
+
+  // ===== Copies =====
 
   for (const dir of DIRECTORIES_TO_COPY) {
-    const from = resolve(CONTENT_PATH, dir);
-    const to = resolve(BUILD_PATH, dir);
-
+    const from = join(source, dir);
+    const to = join(target, dir);
     const entries = await readdir(from, { withFileTypes: true });
+
+    logFileOp(MKDIR, to);
     await mkdir(to, { recursive: true });
 
     for (const entry of entries) {
       if (!entry.isFile()) continue;
 
-      await copyFile(resolve(from, entry.name), resolve(to, entry.name));
+      const from2 = resolve(from, entry.name);
+      const to2 = resolve(to, entry.name);
+
+      logFileOp(COPY, from2, to2);
+      await copyFile(from2, to2);
     }
   }
 
   for (const file of FILES_TO_COPY) {
-    const from = resolve(CONTENT_PATH, file);
-    const to = resolve(BUILD_PATH, file);
+    const from = join(source, file);
+    const to = join(target, file);
 
+    logFileOp(COPY, from, to);
     await copyFile(from, to);
   }
 
-  for (const file of STYLES) {
-    const from = resolve(CONTENT_PATH, file);
-    const to = resolve(BUILD_PATH, file);
+  // ===== Styles =====
 
+  console.error(TOOL);
+  for (const file of STYLES) {
+    const from = join(source, file);
+    const to = join(target, file);
+
+    logFileOp(STYLE, from, to);
     const input = await readFile(from);
     const output = await postcss.process(input, { from, to });
     output.warnings().forEach(warn => {
@@ -80,10 +126,13 @@ async function build() {
     await writeFile(to, output.css);
   }
 
-  for (const file of MODULES) {
-    const from = resolve(CONTENT_PATH, file);
-    const to = resolve(BUILD_PATH, file);
+  // ===== Scripts =====
 
+  for (const file of SCRIPTS) {
+    const from = join(source, file);
+    const to = join(target, file);
+
+    logFileOp(COPY, from, to);
     await copyFile(from, to);
   }
 }
