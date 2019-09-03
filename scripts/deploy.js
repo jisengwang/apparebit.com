5fd31548812674850153ef85d9007b25f12cde0d
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2019-09-03 01:06:58 -0400

    Make deploy script work by using the correct rsync options correctly

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const { TARGET_ROOT } = require('./config.js');
+const source = TARGET_ROOT + (TARGET_ROOT.endsWith('/') ? '' : '/');
 const { spawn } = require('child_process');
 
 function deploy() {
@@ -10,12 +11,33 @@ function deploy() {
     reject = rej;
   });
 
-  spawn('rsync', [
-    '-ruv',
+  // rsync <options> source target
+  //     NB Source path must end with slash to sync contents, not directory.
+  //
+  // -c, --checksum           use checksum not file size and modification time
+  // -e, --rsh=COMMAND        remote shell
+  // -n, --dry-run            dry run
+  // -r, --recursive          recursively
+  // -u, --update             skip destination files with newer timestamp
+  // -v, --verbose            verbose
+  //     --exclude=PATTERN    exclude from source
+  //     --delete             delete from destination
+
+  const options = [
+    '-cruv',
     '-e', 'ssh -p 2222',
-    '--exclude', 'cgi-bin *~ .DS_Store',
+    '--exclude', 'cgi-bin',
+    '--exclude', '.well-known',
+    '--exclude', '.DS_Store',
+    '--exclude', '.git',
     '--delete',
-    TARGET_ROOT,
+  ];
+
+  if (process.argv.includes('--dry-run')) options.push('--dry-run');
+
+  spawn('rsync', [
+    ...options,
+    source,
     'rgrimm@192.232.251.218:public_html/'
   ], {
     stdio: 'inherit',
