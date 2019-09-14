a6c9c0dc6016e94ea522b33b2fb873bb59d75e44
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2019-09-14 08:02:21 -0400

    Add support for hashing inline scripts and adding them to CSP header

--- scripts/htaccess.js
+++ scripts/htaccess.js
@@ -2,17 +2,34 @@
 /* (C) Copyright 2019 Robert Grimm. Released under MIT license. */
 'use strict';
 
-const { CONFIG_FILES } = require('./config.js');
+const { CONFIG_FILES, SOURCE_ROOT } = require('./config.js');
+const { createHash } = require('crypto');
+/* Don't use `.` for text between tags since it doesn't match newlines. */
+const INLINE_SCRIPT = /<script>([\s\S]*?)<\/script>/u;
 const { join } = require('path');
+const { readFile, writeFile } = require('fs').promises;
 const { spawn } = require('child_process');
 const { logger } = require('./util');
 const log = logger('htaccess');
 
+const DOT_HTACCESS = join(CONFIG_FILES, '.htaccess');
 const BUILD_SH = join(
   __dirname, '..', '..', 'server-configs-apache', 'bin', 'build.sh'
 );
 
-function htaccess() {
+function hash(s) {
+  const h = createHash('sha256').update(s, 'utf8').digest('base64');
+  return `'sha256-${h}'`;
+}
+
+function hashInlineScripts(html) {
+  return [...html.matchAll(INLINE_SCRIPT)]
+    .map(match => match[1])
+    .map(hash)
+    .join(' ');
+}
+
+function build() {
   let resolve, reject, promise = new Promise((res, rej) => {
     resolve = res;
     reject = rej;
@@ -32,6 +49,20 @@ async function htaccess() {
   // 1. Build the .htaccess configuration.
   log(`Build .htaccess`);
   await build();
+
+  // 2. Determine SHA-256 hash for each inline script element.
+  log(); log(`Hash inline scripts`);
+  const frontpage = await readFile(join(SOURCE_ROOT, 'index.html'), 'utf8');
+  const hashes = hashInlineScripts(frontpage);
+
+  // 3. Inject into .htaccess configuration.
+  log(`Inject into .htaccess`);
+  let config = await readFile(DOT_HTACCESS, 'utf8');
+  config = config.replace(
+    `script-src 'self' www.google-analytics.com;`,
+    `script-src 'self' ${hashes} www.google-analytics.com;`
+  );
+  await writeFile(DOT_HTACCESS, config, 'utf8');
 }
 
 if (require.main === module) {
