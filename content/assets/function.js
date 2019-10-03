9614b693e6df75310b78942bdf87ad3a43cb5856
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2019-10-03 04:34:02 -0400

    Switch to ES6
    ES6 became available in MS Edge in April 2017, Firefox in June 2017, Google Chrome in May 2016, and Safari in September 2016.

--- content/assets/function.js
+++ content/assets/function.js
@@ -1,37 +1,39 @@
 /* ========================================================================== *
  * (C) Copyright 2019 Robert Grimm, released under the MIT license.
  * ========================================================================== */
+'use strict';
 
-var options = document.currentScript.dataset;
+const { max, round } = Math;
+const options = document.currentScript.dataset;
 
 function addReferencesFooter() {
   /* Only add references if article doesn't already have a footer with them. */
-  var article = document.querySelector('main > article');
+  const article = document.querySelector('main > article');
   if (!article || article.querySelector('footer.references')) return;
 
   /* Create the new footer containing the references. */
-  var footer = document.createElement('footer');
+  const footer = document.createElement('footer');
   footer.classList.add('references');
 
-  var h2 = document.createElement('h2');
+  const h2 = document.createElement('h2');
   h2.innerText = 'References';
   if (options.h2) h2.className = options.h2;
-  var div = document.createElement('div');
+  const div = document.createElement('div');
   div.classList.add('heading');
   div.appendChild(h2);
   footer.appendChild(div);
 
-  var ol = document.createElement('ol');
+  const ol = document.createElement('ol');
   footer.appendChild(ol);
 
-  var hyperlinks = article.querySelectorAll('a[href]');
-  for (var i=0; i<hyperlinks.length; i++) {
-    var href = hyperlinks[i].href;
-    if (!href.startsWith('https://apparebit.com')) {
-      var link = document.createElement('a');
+  const { origin } = window;
+  const hyperlinks = article.querySelectorAll('a[href]');
+  for (const href of hyperlinks) {
+    if (!href.startsWith(origin)) {
+      const link = document.createElement('a');
       link.href = href;
       link.innerText = href;
-      var li = document.createElement('li');
+      const li = document.createElement('li');
       li.appendChild(link);
       ol.appendChild(li);
     }
@@ -44,8 +46,8 @@ function addReferencesFooter() {
 /* Only add references iff the body has class `print-references`. */
 if (document.body.classList.contains('print-references')) {
   window.onbeforeprint = addReferencesFooter;
-  var queries = window.matchMedia('print');
-  queries.addListener(function(mql) {
+  const queries = window.matchMedia('print');
+  queries.addListener(mql => {
     if (mql.matches) {
       addReferencesFooter();
     }
