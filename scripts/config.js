61675cf480921cc09983abaf4ee80cff01585cd0
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2019-10-06 01:48:22 -0400

    Separate configuration files from content files
    Also rename corresponding collections so that names are more consistent.

--- scripts/config.js
+++ scripts/config.js
@@ -6,10 +6,10 @@ const ROOT = resolve(__dirname, '..');
 
 module.exports = {
   ROOT,
-  CONFIG_FILES: join(ROOT, 'config'),
+  CONFIG_ROOT: join(ROOT, 'config'),
   SOURCE_ROOT: join(ROOT, 'content'),
   TARGET_ROOT: join(ROOT, 'build'),
-  DIRECTORIES_TO_COPY: [
+  CONTENT_DIRECTORIES_TO_COPY: [
     'about',
     'assets/fonts',
     'assets/icons',
@@ -17,15 +17,17 @@ module.exports = {
     'features/do-you-be',
     'features/ubu-trump',
   ],
-  FILES_TO_COPY: [
+  CONTENT_FILES_TO_COPY: [
     '403.html',
     '404.html',
     'index.html',
     'robots.txt',
     'assets/apparebit.webmanifest',
-    'config/.htaccess',
-    'config/aarrh6dhlwkj38ie8h9y1pmlzg3gyz.html',
-    'config/googlee753ef1b651640a9.html',
+  ],
+  CONFIG_FILES_TO_COPY: [
+    '.htaccess',
+    'aarrh6dhlwkj38ie8h9y1pmlzg3gyz.html',
+    'googlee753ef1b651640a9.html',
   ],
   MARKUP: [
     '403.html',
