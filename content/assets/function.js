f84beed7c87b6c78838c202a7d26b3cefe8f572e
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2019-10-23 20:08:43 -0400

    Make more robust; enable better compression

--- content/assets/function.js
+++ content/assets/function.js
@@ -3,51 +3,72 @@
  * ========================================================================== */
 'use strict';
 
-const options = document.currentScript.dataset;
+const DEBUG = false;
 
-function addReferencesFooter() {
-  // >>> Phase 1: READ from DOM. If references don't exist, extract links.
-  const article = document.querySelector('main > article');
-  if (!article || article.querySelector('footer.references')) return;
-  const hyperlinks = article.querySelectorAll('a[href]');
+(function main(window, document) {
+  const options = document.currentScript.dataset;
 
-  // >>> Phase 2: WRITE to DOM. Create mark-up for footer with references.
-  const footer = document.createElement('footer');
-  footer.classList.add('references');
+  function createFooterWithReferences() {
+    if (DEBUG) console.log('called createFooterWithReferences()');
 
-  const h2 = document.createElement('h2');
-  h2.innerText = 'References';
-  if (options.h2) h2.className = options.h2;
-  const div = document.createElement('div');
-  div.classList.add('heading');
-  div.appendChild(h2);
-  footer.appendChild(div);
+    // >>> (1) Read from DOM: If footer doesn't exist, extract all links.
+    const article = document.querySelector('main > article');
+    if (!article || article.querySelector('footer.references')) return;
+    const hyperlinks = article.querySelectorAll('a[href]');
 
-  const ol = document.createElement('ol');
-  footer.appendChild(ol);
+    if (DEBUG) {
+      console.log(`processing ${hyperlinks.length} references`);
+    }
 
-  for (const { href } of hyperlinks) {
-    if (!href.startsWith('https://apparebit.com/')) {
-      const link = document.createElement('a');
-      link.href = href;
-      link.innerText = href;
-      const li = document.createElement('li');
-      li.appendChild(link);
-      ol.appendChild(li);
+    // >>> (2) Create DOM: Build up footer with references.
+    const footer = document.createElement('footer');
+    footer.className = 'references';
+
+    const h2 = document.createElement('h2');
+    if (options.h2) h2.className = options.h2;
+    h2.innerText = 'References';
+
+    const div = document.createElement('div');
+    div.className = 'heading';
+    div.appendChild(h2);
+    footer.appendChild(div);
+
+    const ol = document.createElement('ol');
+    footer.appendChild(ol);
+
+    for (const { href } of hyperlinks) {
+      if (!href.startsWith('https://apparebit.com/')) {
+        const link = document.createElement('a');
+        link.href = href;
+        link.innerText = href;
+
+        const li = document.createElement('li');
+        li.appendChild(link);
+        ol.appendChild(li);
+      }
+    }
+
+    // >>> (3) Write to DOM: Add footer to article.
+    article.appendChild(footer);
+  }
+
+  // Once the DOM has loaded, wire up event handlers.
+  function wireUp() {
+    // Safari doesn't support beforeprint event, so media query serves as fallback.
+    // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeprint
+    if (document.body.classList.contains('print-references')) {
+      window.addEventListener('beforeprint', createFooterWithReferences);
+      window.matchMedia('print').addListener(event => {
+        if (event.matches) {
+          createFooterWithReferences();
+        }
+      });
     }
   }
 
-  /* Add footer to article. */
-  article.appendChild(footer);
-}
-
-/* Only add references iff the body has class `print-references`. */
-if (document.body.classList.contains('print-references')) {
-  window.onbeforeprint = addReferencesFooter;
-  const query = window.matchMedia('print');
-  query.addListener(event => {
-    if (event.matches) {
-      addReferencesFooter();
-    }
-  });
-}
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', wireUp);
+  } else {
+    wireUp();
+  }
+})(window, document);
