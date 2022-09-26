55327d934b6c03ffc1eb0b9867d88f47b6c3bb35
Robert Grimm <apparebit@gmail.com>
2022-09-25 21:14:00 -0400

    Add script for running development server, kill already running server

---
+++ script/develop.sh
@@ -0,0 +1,7 @@
+pid=$(lsof -nP -i4TCP:8080 | grep LISTEN | cut -w -f 2)
+if [ -n "$pid" ]; then
+    echo 'killing running server'
+    kill -9 $pid
+fi
+
+"../siteforge/cli/main.js" build develop -vv
