11ef9500550b403ddf67ea0893dcdfdb87425ec4
Robert Grimm <apparebit@gmail.com>
2022-11-23 18:22:43 -0500

    Add rel=me to relevant links on homepage; add Mastodon identity

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,6 +1,7 @@
 /* © 2020-2022 Robert Grimm */
 
-export default function Footer() {
+export default function Footer(file, context) {
+  const me = file.coolPath === '' ? 'me ' : '';
   return `<footer class=page-footer role=contentinfo>
 <div class=in-here>
 <h2 id=in-here>In Here</h2>
@@ -16,16 +17,17 @@ export default function Footer() {
 <div class=out-there>
 <h2 id=out-there>Out There</h2>
 <ul role=list>
-  <li><a rel="author noreferrer" href="https://www.facebook.com/apparebit">Facebook</a></li>
-  <li><a rel=author href="https://github.com/apparebit">Github</a></li>
-  <li><a rel="author noreferrer" href="https://www.instagram.com/apparebit/">Instagram</a></li>
-  <li><a rel="author noreferrer" href="https://www.linkedin.com/in/apparebit/">LinkedIn</a></li>
-  <li><a rel="author noreferrer" href="https://twitter.com/apparebit/">Twitter</a></li>
+  <li><a rel="${me}author noreferrer" href="https://www.facebook.com/apparebit">Facebook</a></li>
+  <li><a rel="${me}author" href="https://github.com/apparebit">Github</a></li>
+  <li><a rel="${me}author noreferrer" href="https://www.instagram.com/apparebit/">Instagram</a></li>
+  <li><a rel="${me}author noreferrer" href="https://www.linkedin.com/in/apparebit/">LinkedIn</a></li>
+  <li><a rel="${me}author noreferrer" href="https://mastodon.social/@apparebit">Mastodon</a></li>
+  <li><a rel="${me}author noreferrer" href="https://twitter.com/apparebit/">Twitter</a></li>
 </ul>
 </div>
 
 <p class=copyright>© 2011–2022
-<a rel="me author" href="mailto:apparebit@gmail.com">Robert Grimm</a>.
+<a rel="${me}author" href="mailto:apparebit@gmail.com">Robert Grimm</a>.
 All rights reserved.</p>
 </footer>`;
 }
